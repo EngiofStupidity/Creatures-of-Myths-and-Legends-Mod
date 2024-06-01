312300412717055d94d2e2ce94a6a4b3a50d58dc
OlahTibor2005 <77668274+TiborOlah2005@users.noreply.github.com>
2024-06-01 09:30:36 +0200

    Initial commit

---
+++ src/client/java/ensil/comal/CreaturesOfMythsAndLegendsModClient.java
@@ -0,0 +1,10 @@
+package ensil.comal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreaturesOfMythsAndLegendsModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
